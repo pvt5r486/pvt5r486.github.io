5c9664d9cfafa7c18ef383774618966d18f789a7
Yuhsuan Tsai <yuhsuan@infinitiessoft.com>
2019-08-09 16:42:45 +0800

    Site updated: 2019-08-09 16:42:00

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
-// build time:Wed Jul 24 2019 11:32:18 GMT+0800 (GMT+08:00)
+// build time:Fri Aug 09 2019 16:41:08 GMT+0800 (GMT+08:00)
 var izCacheVer="1";importScripts("https://cdn.izooto.com/scripts/workers/8a986b5e8fc4d3c2abb21daa25d04fbfeb2ac1f4.js");
 //rebuild by neat 
