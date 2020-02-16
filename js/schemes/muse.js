bf12d7943a2bffde85f1bcd382e5173d41950752
Yuhsuan Tsai <yuhsuan@infinitiessoft.com>
2020-02-16 18:17:24 +0800

    Site updated: 2020-02-16 18:16:40

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sun Oct 20 2019 22:09:09 GMT+0800 (GMT+08:00)
+// build time:Sun Feb 16 2020 18:16:24 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
-// build time:Sun Oct 20 2019 22:08:42 GMT+0800 (GMT+08:00)
+// build time:Sun Feb 16 2020 18:15:57 GMT+0800 (GMT+08:00)
 var izCacheVer="1";importScripts("https://cdn.izooto.com/scripts/workers/8a986b5e8fc4d3c2abb21daa25d04fbfeb2ac1f4.js");
 //rebuild by neat 
