58f2cf37bd093f985e32f4881a44cd02251cd158
Yuhsuan Tsai <yuhsuan@infinitiessoft.com>
2020-03-06 16:06:30 +0800

    Site updated: 2020-03-06 16:05:56

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 06 2020 15:57:51 GMT+0800 (GMT+08:00)
+// build time:Fri Mar 06 2020 16:04:52 GMT+0800 (GMT+08:00)
 var izCacheVer="1";importScripts("https://cdn.izooto.com/scripts/workers/8a986b5e8fc4d3c2abb21daa25d04fbfeb2ac1f4.js");
 //rebuild by neat 
