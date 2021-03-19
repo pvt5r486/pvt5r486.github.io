d82463a2cf832fa9b60187b4d7193550c3c73ee8
pvt5r486 <yuhsuan@infinitiessoft.com>
2021-03-19 17:07:33 +0800

    Site updated: 2021-03-19 17:06:46

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 19 2021 16:55:18 GMT+0800 (GMT+08:00)
+// build time:Fri Mar 19 2021 17:06:13 GMT+0800 (GMT+08:00)
 var izCacheVer="1";importScripts("https://cdn.izooto.com/scripts/workers/8a986b5e8fc4d3c2abb21daa25d04fbfeb2ac1f4.js");
 //rebuild by neat 
