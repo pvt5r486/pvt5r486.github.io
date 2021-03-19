a115deff1d220d336455abc77ac482da094f5ba0
pvt5r486 <yuhsuan@infinitiessoft.com>
2021-03-19 17:30:37 +0800

    Site updated: 2021-03-19 17:29:54

--- js/scroll-cookie.js
+++ js/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri Mar 19 2021 17:23:23 GMT+0800 (GMT+08:00)
+// build time:Fri Mar 19 2021 17:29:48 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
